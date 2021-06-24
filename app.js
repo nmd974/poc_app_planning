7fb013f8f277b1f06de69b9e130f1815833bae48
nmd974 <jha.payet@gmail.com>
2021-06-24 10:57:13 +0400

    ajout de la requete

--- app.js
+++ app.js
@@ -7,34 +7,12 @@ const app = Vue.createApp({
   methods:{
     async fetchData () {
       try {
-        var response = await axios.get(`http://127.0.0.1:80/activities/1`);
-        console.log(response.data);
+        var response = await axios.get(`http://127.0.0.1:80/planning/1`);
+        console.log(typeof response.data);
         this.planning = response.data;
       } catch (error) {
         console.log(error);
       }
-      // this.planning = axios.get(`http://127.0.0.1:80/activities/1`).then();
-      // console.log(axios.get(`http://127.0.0.1:80/activities/1`));
-      // let xhr = new XMLHttpRequest();
-      // xhr.open("GET", `http://127.0.0.1:80/activities/1`);
-      // xhr.responseType = "json";
-      // xhr.send();
-      // const that = this;
-      // xhr.onload = function(){
-      //     if (xhr.status != 200){ 
-      //         alert("Erreur " + xhr.status + " : " + xhr.statusText);
-      //     }else{ 
-      //         console.log(xhr);
-      //         var response = xhr.response;
-      //         response.forEach(element => {
-      //           that.planning.push(element);
-      //         });
-              
-      //     } 
-      // };
-      // xhr.onerror = function(){
-      //     alert("La requête a échoué");
-      // };
     }
   }
 });
