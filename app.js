a95295af1f6507303ffaf047792de714eeed7c2f
nmd974 <jha.payet@gmail.com>
2021-06-25 11:03:13 +0400

    ajout de la mise en forme du retour des informations

--- app.js
+++ app.js
@@ -4,12 +4,30 @@ const app = Vue.createApp({
       planning: [] 
     };
   },
+  watch:{
+    planning(value){
+      return value;
+    }
+  },
+  // computed:{
+  //   display(){
+  //     this.fetchData();
+  //     if(this.planning.length > 0){
+  //       return this.planning;
+  //     }else{
+  //       this.planning.push("Vous n'avez pas d'activités aujourd'hui");
+  //     }
+  //   }
+  // },
   methods:{
     async fetchData () {
       try {
         var response = await axios.get(`http://127.0.0.1:80/planning/1`);
-        console.log(typeof response.data);
-        this.planning = response.data;
+        response.data.forEach(el => {
+          console.log(el);
+          this.planning.push(el);
+        })
+        console.log(this.planning);
       } catch (error) {
         console.log(error);
       }
