217f95158166f259b0a8fd70519ebdc2c4d91e42
nmd974 <jha.payet@gmail.com>
2021-06-25 11:45:19 +0400

    ajout du traitement du token

--- app.js
+++ app.js
@@ -4,25 +4,13 @@ const app = Vue.createApp({
       planning: [] 
     };
   },
-  watch:{
-    planning(value){
-      return value;
-    }
+  mounted(){
+    this.fetchData();
   },
-  // computed:{
-  //   display(){
-  //     this.fetchData();
-  //     if(this.planning.length > 0){
-  //       return this.planning;
-  //     }else{
-  //       this.planning.push("Vous n'avez pas d'activités aujourd'hui");
-  //     }
-  //   }
-  // },
   methods:{
     async fetchData () {
       try {
-        var response = await axios.get(`http://127.0.0.1:80/planning/1`);
+        var response = await axios.get(`http://127.0.0.1:80/planning/${document.location.search.slice(3)}`);
         response.data.forEach(el => {
           console.log(el);
           this.planning.push(el);
