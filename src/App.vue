c71af7dd1cec009e7d4376c404d00e8f1a244187
nmd974 <jha.payet@gmail.com>
2021-07-27 09:03:58 +0400

    ajout de la version CLI

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Planning />
+</template>
+
+<script>
+import Planning from './components/Planning.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Planning
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Planning.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="planning" class="container">
+    <div v-if="planning.length > 0" class="mb-5 display-2">
+      Bonjour, {{ planning[0].first_name}} {{ planning[0].last_name}}
+    </div>
+    <ul class="list-group" >
+      <li 
+        class="list-group-item disabled d-flex justify-content-between w-100" 
+        v-show="planning.length > 0" 
+        v-for="plan in planning" v-bind:key="plan">
+          <div>{{ plan.label }} </div>
+          <div>{{ plan.start.substr(11,8) }} - {{ plan.end.substr(11,8) }} </div>
+      </li>
+      <li class="list-group-item disabled" v-if="planning.length === 0">Vous n'avez pas d'activités prévues aujourd'hui</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return { 
+      planning: [] 
+    };
+  },
+  mounted(){
+    this.fetchData();
+  },
+  methods:{
+    async fetchData () {
+      try {
+        var response = await axios.get(`http://127.0.0.1:80/planning/${document.location.search.slice(3)}`);
+        response.data.forEach(el => {
+          console.log(el);
+          this.planning.push(el);
+        })
+        console.log(this.planning);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
