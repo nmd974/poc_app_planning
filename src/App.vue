8db9b2afa5a6040bb725388fe39f0cf0245fcc18
Gilles974 <boyer.gilles@live.fr>
2021-08-24 20:11:35 +0400

    test prob router

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
         <!-- If using vue-router -->
-        toto
         <router-view></router-view>
       </v-container>
     </v-main>
